5c5cc4189c52ca9d7b730a6a75574b95241a826d
davay <davay@devinl.im>
2020-03-06 20:16:41 -0800

    progress on create, and cleanup

--- client.py
+++ client.py
@@ -3,11 +3,13 @@
 import sys
 import os
 import os.path
+import boto3
 from os import path
-from fsplit.filesplit import FileSplit
+from fsplit.filesplit import FileSplit #pip3 install filesplit or pip2 install fsplit
+from flask import request
 
 #Download folder
-CONST_DOWN = "\\Downloads\\" 
+CONST_DOWN = "Downloads/" 
 
 #Console Interface
 CONST_HELP = "help"
@@ -26,13 +28,12 @@ CONST_BLOCK_SIZE = .5
 #Bytes per MB
 CONST_BYTES_PER_MB = 1000000
 
+bucket = boto3.resource('s3').Bucket("termprojbucket")
 
 def main():
     initDirs()
     
-    
-    
-    
+    bucket.download_file("Test1.txt", CONST_DOWN+"test.jfif")
     ########TESTING########
     testFile = "test.jfif" 
     testOutput = "out.jfif"
@@ -45,10 +46,6 @@ def main():
     
     deleteBlocks(getBlockNames(testFile))
     #######################
-
-
-
-
     
     active = True
     while active:
@@ -59,11 +56,11 @@ def main():
             active = False
         elif inputStr == CONST_HELP:
             print("usage:")
-            print("create bucket_name s3_file_path")
+            print("create s3_file_name")
             print("read SUFS_file_name")
             print("list SUFS_file_name")
         elif inList[0]  == CONST_CREATE:
-            createCMD(inList[1], inList[2])
+            createCMD(inList[1], bucket) #have to pass in object bucket here so its reachable
         elif inList[0] == CONST_READ:
             readCMD(inList[1])
         elif inList[0] == CONST_LIST:
@@ -77,7 +74,18 @@ def main():
 
 #$create "filePath" "buckName"
 #Write specified file from s3 BUCKET into SUFS
-def createCMD(path, bucket):
+def createCMD(filename, bucket):
+    output_path = CONST_DOWN + "file"
+    #1. download file from s3
+    bucket.download_file(filename, output_path) 
+    #2. split file
+    splitFile("file")
+    #3. contact namenode (???????)
+    
+    #4. get datanode addresses 
+
+    #5. submit http requests to datanodes directly, switching in a round robin way
+
     print(path)
     print(bucket)
 
@@ -96,14 +104,14 @@ def listCMD(filename):
 #Splits a file in the downloads folder into blocks
 #Original file deleted
 def splitFile(fileName):
-    fs = FileSplit(file=os.getcwd() + CONST_DOWN + fileName, splitsize=CONST_BLOCK_SIZE * CONST_BYTES_PER_MB, output_dir=(os.getcwd() + CONST_DOWN))
+    fs = FileSplit(file=CONST_DOWN + fileName, splitsize=CONST_BLOCK_SIZE * CONST_BYTES_PER_MB, output_dir=(CONST_DOWN))
     fs.split()
-    os.remove(os.getcwd() + CONST_DOWN + fileName)
+    os.remove(CONST_DOWN + fileName)
 
 #Merge all blocks of the 
 #Merges in order of fileNames array and result is placed in Downloads folder
 def mergeFile(blockNameArray, fileName):
-    with open(os.getcwd() + CONST_DOWN + fileName, 'wb') as merged:
+    with open(CONST_DOWN + fileName, 'wb') as merged:
         for blockName in blockNameArray:
             with open(blockName, 'rb') as currBlock:
                 merged.write(currBlock.read())            
@@ -120,21 +128,16 @@ def getBlockNames(fileName):
     nameArray = fileName.rsplit('.', 1)    
 
     i = 1
-    while(path.exists(os.getcwd() + CONST_DOWN + nameArray[0] + "_" + str(i) + "." + nameArray[1])):
-        blockList.append(os.getcwd() + CONST_DOWN + nameArray[0] + "_" + str(i) + "." + nameArray[1])
+    while(path.exists(CONST_DOWN + nameArray[0] + "_" + str(i) + "." + nameArray[1])):
+        blockList.append(CONST_DOWN + nameArray[0] + "_" + str(i) + "." + nameArray[1])
         i +=1
     
     return blockList
 
 #Create downloads folder if it does not exits
 def initDirs():   
-    if not os.path.exists(os.getcwd() + CONST_DOWN):
-        os.makedirs(os.getcwd() + CONST_DOWN)
-    
-    
-
-    
-
+    if not os.path.exists(CONST_DOWN):
+        os.makedirs(CONST_DOWN)
    
 if __name__ == '__main__':
     main()
