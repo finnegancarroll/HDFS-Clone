caf337317ccfe8137cb89619d33f36bbb3ff488e
EC2 Default User <ec2-user@ip-172-31-26-21.us-west-2.compute.internal>
2020-03-07 21:59:34 +0000

    datanode bugfix

--- Datanode/datanode.py
+++ Datanode/datanode.py
@@ -1,6 +1,8 @@
-import flask
 from flask import Flask, request
+import requests
+import os.path
 import boto3
+import flask
 import os
 
 app = Flask(__name__)
@@ -25,14 +27,47 @@ def writeBlock():
     i = 0
     while i < int(numBlocks):
         i += 1
-        f = request.files[splitName[0] + '_' + str(i) + '.' + splitName[1]]
-        print(splitName[0] + '_' + str(i) + '.' +  splitName[1])
+        f = request.files[splitName[0] + '_' + str(i) + '.' + splitName[1]]1
         #Save block to upload dir with block name
         f.save(os.path.join(UPLOAD_DIRECTORY, splitName[0] + '_' + str(i) + '.' + splitName[1]))
-    
+
     print("Wrote new blocks")
+    
+    
+    #
+    blockList = getBlockNames(fileName)
+    print(blockList)
+    forwardBlocks(blockList, 1)
+    
     # Return 201 CREATED
     return "", 201
+
+
+#Orders datanodes by DNS and scps file blocks to the next n nodes in the list
+def forwardBlocks(blockList, n):
+    response = requests.get('http://169.254.169.254/latest/meta-data/instance-id')
+    my_id = response.text
+    i = 0
+    
+    addrList = getDatanodeAddressList()
+    
+    
+    for block in blockList:
+        if i < n:
+            print(my_id)
+
+def getBlockNames(fileName):
+    blockList = []
+    
+    #split filename into name and extension
+    nameArray = fileName.rsplit('.', 1)    
+
+    i = 1
+    while(os.path.exists(UPLOAD_DIRECTORY + nameArray[0] + "_" + str(i) + "." + nameArray[1])):
+        blockList.append(UPLOAD_DIRECTORY + nameArray[0] + "_" + str(i) + "." + nameArray[1])
+        i +=1
+    
+    return blockList
 
 @app.route('/blocks/<blockname>', methods=['GET'])
 def readBlock(blockname):
@@ -41,6 +76,21 @@ def readBlock(blockname):
 @app.route('/blocks/<blockname>', methods=['POST'])
 def replicateBlock(blockname):
     print("replicateBlock")
+
+
+#Returns a list of dns addresses for all running datanodes
+def getDatanodeAddressList():
+    addressList = []
+
+    #Filter for running instances
+    instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+    
+    for instance in instances:
+        if instance.tags != None:
+            for tag in instance.tags:
+                if ((tag["Key"] == 'Name') and (tag['Value'] == 'Datanode')):
+                    addressList.append(instance.public_dns_name)
+    return addressList
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8000)
