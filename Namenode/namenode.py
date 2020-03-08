f3b13caef44ab7e29b33ea560d9ea8ce24b2da12
EC2 Default User <ec2-user@ip-172-31-23-237.us-west-2.compute.internal>
2020-03-08 02:30:41 +0000

    heawrtbeat fix

--- Namenode/namenode.py
+++ Namenode/namenode.py
@@ -8,7 +8,6 @@ import threading
 app = Flask(__name__)
 sqs = boto3.resource('sqs')
 heartbeat_queue = sqs.Queue('https://sqs.us-west-2.amazonaws.com/494640831729/heartbeat')
-heartbeat_dict = {"time_since_last_heartbeat": 0, "updated": False} 
 datanodes_dict = {}
 files_dict = {}
 
@@ -18,30 +17,31 @@ def check_queue():
         for message in messages:
             msgDict = json.loads(message.body)
             parse_heartbeat_messages(msgDict)
+
             message.delete()
+        
+        for key in datanodes_dict:
+            if ((datanodes_dict[key])["updated"] == False):
+                (datanodes_dict[key])["time_since_last_heartbeat"] += 1
+            print("ID: " + key + " HEARTBEAT: " + str(datanodes_dict[key]["time_since_last_heartbeat"]))
+            datanodes_dict[key]["updated"] = False      
 
-        for key, value in datanodes_dict.items():
-            if (value["updated"] == False):
-                value["time_since_last_heartbeat"] += 1
-            value["updated"] = False
-            print("ID: " + key + " HEARTBEAT: " + value["time_since_last_heartbeat"])
-
-        print(files_dict)
-        time.sleep(10 - time.time() % 10)
+        time.sleep(20 - time.time() % 20)
 
 def parse_heartbeat_messages(msgDict):
     msgId = msgDict["id"]
 
-    for block in msgDict["blocks"]:
-        blockNameArr = block.rsplit('.',1)
-        fileName = blockNameArr[0].rsplit('_', 1)[0] + "." + blockNameArr[1]
-        
-        if (not files_dict.get(fileName)): files_dict[fileName] = {}
-        if(not files_dict[fileName].get(blockNameArr[0])): files_dict[fileName][blockNameArr[0]] = []
-        if(msgId not in files_dict[fileName][blockNameArr[0]]): files_dict[fileName][blockNameArr[0]].append(msgId)
+    if ("blocks" in msgDict):
+        for block in msgDict["blocks"]:
+            blockNameArr = block.rsplit('.',1)
+            fileName = blockNameArr[0].rsplit('_', 1)[0] + "." + blockNameArr[1]
+            
+            if (not files_dict.get(fileName)): files_dict[fileName] = {}
+            if(not files_dict[fileName].get(blockNameArr[0])): files_dict[fileName][blockNameArr[0]] = []
+            if(msgId not in files_dict[fileName][blockNameArr[0]]): files_dict[fileName][blockNameArr[0]].append(msgId)
 
     if (not datanodes_dict.get(msgId)):
-        datanodes_dict[msgId] = heartbeat_dict
+        datanodes_dict[msgId] = {}
     
     datanodes_dict[msgId]["time_since_last_heartbeat"] = 0
     datanodes_dict[msgId]["updated"] = True
